0cceaddb4d2341150dc420db19e9d3409f688cf6
Ship-of-Agony <287680459+Ship-of-Agony@users.noreply.github.com>
2026-07-30 21:31:09 +0200

    Update C2CamDeleteBackup.sh

--- C2CamDeleteBackup.sh
+++ C2CamDeleteBackup.sh
@@ -1,1 +1,1 @@
-rm -rf /root/c2cam_backup
+rm -rf /root/c2cam_backup && { [ ! -d /root/c2cam_backup ] && echo "===> [SUCCESS] Backup folder removed."; } || echo "===> [ERROR] Backup folder still exists."
